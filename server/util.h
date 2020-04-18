12f66f5878f279028f67274ae1a3bf342eaff891
Justinas Zaliaduonis <justin@Justinass-MBP.wlan.jacobs-university.de>
2020-04-18 23:17:52 +0200

    Add weighted distribution function header

--- server/util.h
+++ server/util.h
@@ -5,3 +5,6 @@ int randomNumberFromInterval(int intervalStart, int intervalEnd);
 
 template <class T>
 T randomElement(const std::vector<T> v);
+
+template <class T>
+std::vector<int> weightedDistribution(std::vector<int> probabilityPairs);
