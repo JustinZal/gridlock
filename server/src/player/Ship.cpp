4c4c0eef95ac4e17b0148ba7d64a2066e03d9ce1
osobiehl <osobiehl@me.com>
2020-06-21 17:18:18 +0200

    added ship class to player folder and CMakelists.txt

---
+++ server/src/player/Ship.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jose Ignacio Biehl on 21.06.20.
+//
+
+#include "Ship.h"
---
+++ server/src/player/Ship.h
@@ -0,0 +1,14 @@
+//
+// Created by Jose Ignacio Biehl on 21.06.20.
+//
+
+#ifndef GRIDLOCK_SHIP_H
+#define GRIDLOCK_SHIP_H
+
+
+class Ship {
+
+};
+
+
+#endif //GRIDLOCK_SHIP_H
